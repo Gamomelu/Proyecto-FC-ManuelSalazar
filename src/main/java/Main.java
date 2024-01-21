2be71435fb9caa9d9f5edb17e7cff06886f0440a
Gamomelu <lsalazar4660@utm.edu.ec>
2024-01-21 12:20:00 -0500

    Comprobacion

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ManuelSalazar
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Imprime la cadena deseada
+        System.out.println("Proyecto Fin De Ciclo - Luis Manuel Salazar Moreira");
+    }
+}
